8e1bfc99bca5920ab2de2515ba44b4cf2bbc5ec2
Quang Hung <duongquanghung0122@gmail.com>
2025-03-27 23:02:13 +0700

    Setting react query tanstack hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,29 @@ import { routes } from './constant/routes';
 import Dashboard from './pages/Dashboard';
 import Layout from './components/layout/Layout';
 import { ToastProvider } from './context/ToastProvider';
+import { QueryClientProvider } from './context/QueryClientProvider';
 
 const App: React.FC = () => {
   return (
     <Suspense fallback={null}>
       <BrowserRouter>
-        <ToastProvider>
-          <Routes>
-            <Route path="/" element={<Navigate to="/dashboard" />} />
-            <Route element={<Dashboard />} />
-            <Route element={<Layout />}>
-              {routes.map((route) => (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  element={route.component}
-                />
-              ))}
-            </Route>
-          </Routes>
-        </ToastProvider>
+        <QueryClientProvider>
+          <ToastProvider>
+            <Routes>
+              <Route path="/" element={<Navigate to="/dashboard" />} />
+              <Route element={<Dashboard />} />
+              <Route element={<Layout />}>
+                {routes.map((route) => (
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    element={route.component}
+                  />
+                ))}
+              </Route>
+            </Routes>
+          </ToastProvider>
+        </QueryClientProvider>
       </BrowserRouter>
     </Suspense>
   );
